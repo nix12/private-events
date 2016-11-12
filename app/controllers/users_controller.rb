33a57d510d42d530e41c05616c44668dc8e4a5e9
Mario Goodwin <mr.goodwin@outlook.com>
2016-11-11 20:41:39 -0500

    User model and signup complete

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(user_params)
+		if @user.save
+			redirect_to @user
+		else
+			flash.now[:danger] = 'Sign up failure'
+			render 'new'
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+
+		def user_params
+			params.require(:user).permit(:name, :email)
+		end
+end
